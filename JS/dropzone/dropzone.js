3f597baef7b38a2a305bb5ab6de4354cca7e98c3
baptistef <baptistefessard@gmail.com>
2018-03-07 23:16:14 +0100

    modification parallelUploads à 2 (anciennement à 20).

--- JS/dropzone/dropzone.js
+++ JS/dropzone/dropzone.js
@@ -174,7 +174,7 @@ var Dropzone = function (_Emitter) {
          * How many file uploads to process in parallel (See the
          * Enqueuing file uploads* documentation section for more info)
          */
-        parallelUploads: 20,
+        parallelUploads: 2,
 
         /**
          * Whether to send multiple files in one request. If
