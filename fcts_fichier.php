6d04d1c0a2603255e6e70eb9d8406053d5d256d1
bfessard <31823399+bfessard@users.noreply.github.com>
2018-03-10 14:57:18 +0100

    retrait var_dump ligne 68

--- fcts_fichier.php
+++ fcts_fichier.php
@@ -65,7 +65,6 @@ class GPS{
             ),
         );
         $return = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $moyLat . ',' . $moyLng . '&sensor=true_or_false&key=AIzaSyACcynPu83SEga6WA9DTJFnGbFg-LDC_Z0',false,stream_context_create($arrContextOptions));
-        var_dump($return);
         $test = json_decode($return, true);
         return $test;
 
