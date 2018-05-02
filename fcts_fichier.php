838259398916e0b3448052bd87d5c974359aa2a2
baptistef <baptistefessard@gmail.com>
2018-05-03 00:25:22 +0200

    Retrait des accents pour les villes et pays.

--- fcts_fichier.php
+++ fcts_fichier.php
@@ -99,12 +99,24 @@ class GPS{
         }
     }
 
-    public static function suppr_accents($str, $encoding='utf-8'){
-                $str=htmlentities($str,ENT_NOQUOTES,$encoding);
-                $str = preg_replace('#&([A-za-z])(?:acute|grave|cedil|circ|orn|ring|slash|th|tilde|uml);#', '\1', $str);
-                $str = preg_replace('#&([A-za-z]{2})(?:lig);#', '\1', $str);
-                $str = preg_replace('#&[^;]+;#', '', $str);
-                return $str;
+    public static function suppr_accents($str){
+        $url = $str;
+        $url = preg_replace('#Ç#', 'C', $url);
+        $url = preg_replace('#ç#', 'c', $url);
+        $url = preg_replace('#è|é|ê|ë#', 'e', $url);
+        $url = preg_replace('#È|É|Ê|Ë#', 'E', $url);
+        $url = preg_replace('#à|á|â|ã|ä|å#', 'a', $url);
+        $url = preg_replace('#@|À|Á|Â|Ã|Ä|Å#', 'A', $url);
+        $url = preg_replace('#ì|í|î|ï#', 'i', $url);
+        $url = preg_replace('#Ì|Í|Î|Ï#', 'I', $url);
+        $url = preg_replace('#ð|ò|ó|ô|õ|ö|ō#', 'o', $url);
+        $url = preg_replace('#Ò|Ó|Ô|Õ|Ö#', 'O', $url);
+        $url = preg_replace('#ù|ú|û|ü#', 'u', $url);
+        $url = preg_replace('#Ù|Ú|Û|Ü#', 'U', $url);
+        $url = preg_replace('#ý|ÿ#', 'y', $url);
+        $url = preg_replace('#Ý#', 'Y', $url);
+
+        return ($url);
     }
 }
 
